26e28d190fa54ec9a99f0b2c707fe04cce817d67
dipak1920basnet <dipak1920basnet@gmail.com>
2025-09-24 01:13:58 +0545

    optimized adjency list

--- graph/adjency_list.py
+++ graph/adjency_list.py
@@ -2,16 +2,17 @@
 class Graph:
     def __init__(self, vertex):
         self.vertex = vertex
-        self.graph = []
+        # builds empty adjency list
+        self.graph = [[] for _ in range(self.vertex + 1)]
 
-    def build_list(self):
-        for _ in range(self.vertex + 1):
-            self.graph.append([])
-
-    def add_vertex(self, vertexs: set, is_directed=False, weight=None):
-        from_, to_ = vertexs
-        if from_ <= 0 or to_ <= 0:
-            print("There should be atlease one vertex")
+    # add vertices to graph
+    def add_vertex(self, edges: tuple[int, int], is_directed=False, weight=None):
+        from_, to_ = edges
+        length = len(self.graph)
+        if from_ not in range(1, length) or to_ not in range(1, length):
+            raise ValueError(
+                f"There edge range should be in between 1 to {self.vertex}"
+            )
 
         if not weight:
             self.graph[from_].append(to_)
@@ -23,11 +24,11 @@ class Graph:
             if not is_directed:
                 self.graph[to_].append((from_, weight))
 
+    # prints the ajdency list
     def print_list(self):
         for i in range(1, len(self.graph)):
-            print(self.graph[i])
+            print(f"{i} -> {self.graph[i]}")
 
 
 new_graph = Graph(4)
-new_graph.build_list()
 new_graph.print_list()
