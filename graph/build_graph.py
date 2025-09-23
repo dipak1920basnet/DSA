65372bfcb0e98cb618f1a28c908e33f2ea01e4d1
dipak1920basnet <dipak1920basnet@gmail.com>
2025-09-23 15:41:42 +0545

    builds graph with all element zero

--- graph/build_graph.py
+++ graph/build_graph.py
@@ -1,11 +1,27 @@
-# Generate matrix 
-new_list = []
-for i in range(4):
-    inside = []
-    for j in range(4):
-        inside.append(0)
-    new_list.append(inside)
+class Graph:
+    def __init__(self, vertex):
+        self.size = vertex
+        self.matrix = []
+        
+    # Generate matrix
+    def build_matrix(self):
+        for i in range(self.size):
+            inside = []
+            for j in range(4):
+                inside.append(0)
+            self.matrix.append(inside)
 
-for j in range(len(new_list)):
-    print(new_list[j])
+    # Print Matrix
+    def print_matrix(self):
+        for j in self.matrix:
+            print(j)
+
+new_graph = Graph(4)
+new_graph.build_matrix()
+new_graph.print_matrix()
+
     
+ 
+
+
+
